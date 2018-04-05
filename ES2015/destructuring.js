6213b3af43771aa18ecd6168283d374f21f138e0
Ondrej Urban <ondrej.urban@plus4u.net>
2018-04-05 14:26:02 +0200

    add more destructuring examples

--- ES2015/destructuring.js
+++ ES2015/destructuring.js
@@ -1,17 +1,42 @@
 // ARRAY DESTRUCTURING
-// simple
 const numbers = [ 1, 2, 3, 4, 5 ];
+
+// simple
 const [ one, two ] = numbers;
 console.log(one, two); // 1 2
 
 // using array rest
-const [ first, second, ...rest ] = numbers;
+let [ first, second, ...rest ] = numbers;
 console.log(first, second); // 1 2
 console.log(rest); // [ 3, 4, 5 ]
 
-// separate declaration
-let a, b;
-[ a, b ] = numbers;
-console.log(a, b); // 1 2
+// assigning to existing variables
+[ first, second ] = numbers;
+console.log(first, second); // 1 2
 
 // default values
+[ first, second, def = "hello" ] = numbers.slice(0, 2); // [ 1, 2 ]
+console.log(first, second, def); // 1 2 'hello'
+
+// OBJECT DESTRUCTURING
+const person = {
+  name: "John",
+  surname: "Doe",
+  age: 24,
+  dog: {
+    breed: "Bulldog",
+    weight: 20,
+  }
+};
+
+// with rest and defaults
+let { name, surname, mother = "Jane", ...other } = person;
+console.log(name, surname, mother, "--->", other); // John Doe Jane ---> { age: 24, friend: { name: 'Maria', surname: 'Poe', age: 28 } }
+
+// deep destructuring
+let { dog: { breed } } = person;
+console.log(breed); // Bulldog
+
+// key renaming
+let {name: firstname} = person;
+console.log(firstname); // John
